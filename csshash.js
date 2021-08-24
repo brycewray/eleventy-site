06bec9caa2186c097b0fbd0e5b391dde6d7a5984
Bryce Wray <bwray@wraytx.com>
2021-08-24 17:54:30 -0500

    Nonces in CSP

--- csshash.js
+++ csshash.js
@@ -1,11 +1,20 @@
 const fs = require('fs')
 const md5 = require('md5')
 const globAll = require('glob-all')
+const NONCEFILE = '_data/nonce.json'
 const DATAFILE = '_data/csshash.json'
 const PCSSFILE = 'csshash'
 cssFiles = globAll.sync([
   'src/assets/css/*.css'
 ])
+var timestamp = new Date().getUTCMilliseconds()
+var randomNo = Math.floor((Math.random() + 2) * timestamp * timestamp)
+var md5Random = md5(randomNo)
+var nonceResult = `nonce-${md5Random}`
+var nonceJSON = `{
+  "nonceValue": "${nonceResult}"
+}`
+fs.writeFileSync(NONCEFILE, nonceJSON)
 
 var cssMd5Total = 0
 var cssContent = ''
@@ -15,6 +24,7 @@ for(i=0; i<cssFiles.length; i++) {
 }
 cssMd5Total = md5(cssContent)
 console.log(`CSS MD5 result =`, cssMd5Total)
+console.log(`Random =`, md5Random)
 
 var jsonValue = `{
   "indexCSS": "index-${cssMd5Total}.css"
