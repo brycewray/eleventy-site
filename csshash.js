656826a8911bfa1f7971e7ca5441ce75da059c06
Bryce Wray <bwray@wraytx.com>
2021-08-24 20:24:19 -0500

    Back to previous situation

--- csshash.js
+++ csshash.js
@@ -1,20 +1,11 @@
 const fs = require('fs')
 const md5 = require('md5')
 const globAll = require('glob-all')
-const NONCEFILE = '_data/nonce.json'
 const DATAFILE = '_data/csshash.json'
 const PCSSFILE = 'csshash'
 cssFiles = globAll.sync([
   'src/assets/css/*.css'
 ])
-var timestamp = new Date().getUTCMilliseconds()
-var randomNo = Math.floor((Math.random() + 2) * timestamp * timestamp)
-var md5Random = md5(randomNo)
-var nonceResult = `nonce-${md5Random}`
-var nonceJSON = `{
-  "nonceValue": "${nonceResult}"
-}`
-fs.writeFileSync(NONCEFILE, nonceJSON)
 
 var cssMd5Total = 0
 var cssContent = ''
@@ -24,7 +15,6 @@ for(i=0; i<cssFiles.length; i++) {
 }
 cssMd5Total = md5(cssContent)
 console.log(`CSS MD5 result =`, cssMd5Total)
-console.log(`Random =`, md5Random)
 
 var jsonValue = `{
   "indexCSS": "index-${cssMd5Total}.css"
