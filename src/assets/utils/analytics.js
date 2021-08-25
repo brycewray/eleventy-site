0c6774b5061af33c009fd755077c52ae264b8080
Bryce Wray <bwray@wraytx.com>
2021-08-25 03:52:12 -0500

    Prep for nonce-worker.js

--- src/assets/utils/analytics.js
+++ src/assets/utils/analytics.js
@@ -4,8 +4,8 @@ var analyticsCode = ``
 metadata.domain == "brycewray.com"
   ? analyticsCode = /*HTML*/`
   <!-- Fathom - beautiful, simple website analytics -->
-  <script src="https://boa.brycewray.com/script.js" site="CSERHYIA" included-domains="brycewray.com,www.brycewray.com" defer></script>
-  <!-- / Fathom --> 
+  <script nonce="DhcnhD3khTMePgXw" src="https://boa.brycewray.com/script.js" site="CSERHYIA" included-domains="brycewray.com,www.brycewray.com" defer></script>
+  <!-- / Fathom -->
   `
   : analyticsCode = ``
 
