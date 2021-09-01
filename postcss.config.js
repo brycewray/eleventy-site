123bb274431452e6af7df5b3de79d6027c55a66e
Bryce Wray <bwray@wraytx.com>
2021-09-01 15:19:09 -0500

    Adding PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-csso')({
       restructure: true
+    }),
+    purgecss({
+      content: [
+        './**/*.html',
+        './**/*.njk'
+      ]
     })
   ],
 }
