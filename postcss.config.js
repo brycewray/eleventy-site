d0767d6e0336ab9d2ad745e810b8c4fe7e4d7fb3
Bryce Wray <bwray@wraytx.com>
2021-09-01 15:26:24 -0500

    Remove PurgeCSS (no post-Brotli difference)

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,8 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   plugins: [
     require('autoprefixer'),
     require('postcss-csso')({
       restructure: true
-    }),
-    purgecss({
-      content: [
-        './**/*.html',
-        './**/*.njk'
-      ]
     })
   ],
 }
