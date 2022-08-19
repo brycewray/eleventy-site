bc0a1379e85c86c9a9dcf30bf9ffcad62e47f883
Bryce Wray <bwray@wraytx.com>
2022-08-19 16:00:15 -0500

    Further stop pt, 2022-08-19-1600CDT

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss")
 const svgContents = require("eleventy-plugin-svg-contents")
 const path = require('path')
 const Image = require("@11ty/eleventy-img")
-// const pluginEmbedTweet = require("eleventy-plugin-embed-tweet")
+const pluginEmbedTweet = require("eleventy-plugin-embed-tweet")
 
 async function imageShortcode(src, alt) {
   let sizes = "(min-width: 1024px) 100vw, 50vw"
@@ -53,9 +53,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss)
   eleventyConfig.addPlugin(svgContents)
 
-  // eleventyConfig.addPlugin(pluginEmbedTweet, {
-  //   useInlineStyles: false,
-  // })
+  eleventyConfig.addPlugin(pluginEmbedTweet, {
+    useInlineStyles: false,
+  })
 
   eleventyConfig.setQuietMode(true)
 
@@ -172,33 +172,33 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/assets/scss/*.scss")
 
 	// --- REQUIRES Eleventy v.2.0+
-	// eleventyConfig.setServerOptions({
-	// 	enabled: true, // default
-  //   port: 3000, // default is 8080
-  //   showAllHosts: true,
-  //   showVersion: true
-  // })
+	eleventyConfig.setServerOptions({
+		// enabled: true, // default
+    port: 3000, // default is 8080
+    showAllHosts: true,
+    showVersion: true
+  })
 
 	// BrowserSync stuff
 	// -- **TURN OFF** in Eleventy v.2.0+
-  eleventyConfig.setBrowserSyncConfig({
-    ...eleventyConfig.browserSyncConfig,
-    ghostMode: false, // the default as of 1.0.x
-    port: 3000,
-    // callbacks: {
-    //   ready: function(err, bs) {
-    //     bs.addMiddleware("*", (req, res) => {
-    //       const content_404 = fs.readFileSync('_site/404.njk')
-    //       // Add 404 http status code in request header.
-    //       res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
-    //       // Provides the 404 content without redirect.
-    //       res.write(content_404)
-    //       res.end()
-    //     })
-    //   }
-    // },
-    // snippet: false,
-  })
+  // eleventyConfig.setBrowserSyncConfig({
+  //   ...eleventyConfig.browserSyncConfig,
+  //   ghostMode: false, // the default as of 1.0.x
+  //   port: 3000,
+  //   // callbacks: {
+  //   //   ready: function(err, bs) {
+  //   //     bs.addMiddleware("*", (req, res) => {
+  //   //       const content_404 = fs.readFileSync('_site/404.njk')
+  //   //       // Add 404 http status code in request header.
+  //   //       res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
+  //   //       // Provides the 404 content without redirect.
+  //   //       res.write(content_404)
+  //   //       res.end()
+  //   //     })
+  //   //   }
+  //   // },
+  //   // snippet: false,
+  // })
 
   eleventyConfig.addNunjucksAsyncShortcode(
     "imgc",
--- src/assets/js/copy-code-button.js
+++ src/assets/js/copy-code-button.js
@@ -13,7 +13,7 @@ const svgCheck =
 
 const addCopyButtons = (clipboard) => {
 	// 1. Look for pre > code elements in the DOM
-	document.querySelectorAll("pre > code").forEach((codeBlock) => {
+	document.querySelectorAll('pre[class*="language-"]').forEach((codeBlock) => {
 		// 2. Create a button that will trigger a copy operation
 		const button = document.createElement("button");
 		button.className = "clipboard-button";
@@ -29,8 +29,8 @@ const addCopyButtons = (clipboard) => {
 				(error) => (button.innerHTML = "Error")
 			);
 		});
-		// 3. Append the button directly before the pre tag
-		const pre = codeBlock.parentNode;
+		// 3. Append the button directly before the code tag (for PrismJS)
+		const pre = codeBlock.firstChild;
 		pre.parentNode.insertBefore(button, pre);
 	});
 };
