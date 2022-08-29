c1a64eed2656d4d5baaeb8f68988a8ad3cf85e09
Bryce Wray <bwray@wraytx.com>
2022-08-29 04:34:26 -0500

    While switching between 1.x and 2.x canary

--- .eleventy.js
+++ .eleventy.js
@@ -186,24 +186,24 @@ module.exports = function(eleventyConfig) {
 
 	// BrowserSync stuff
 	// -- "no-op" in Eleventy v.2.0+
-  eleventyConfig.setBrowserSyncConfig({
-    ...eleventyConfig.browserSyncConfig,
-    ghostMode: false, // the default as of 1.0.x
-    port: 3000,
-    // callbacks: {
-    //   ready: function(err, bs) {
-    //     bs.addMiddleware("*", (req, res) => {
-    //       const content_404 = fs.readFileSync('_site/404.njk')
-    //       // Add 404 http status code in request header.
-    //       res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
-    //       // Provides the 404 content without redirect.
-    //       res.write(content_404)
-    //       res.end()
-    //     })
-    //   }
-    // },
-    // snippet: false,
-  })
+  // eleventyConfig.setBrowserSyncConfig({
+  //   ...eleventyConfig.browserSyncConfig,
+  //   ghostMode: false, // the default as of 1.0.x
+  //   port: 3000,
+  //   // callbacks: {
+  //   //   ready: function(err, bs) {
+  //   //     bs.addMiddleware("*", (req, res) => {
+  //   //       const content_404 = fs.readFileSync('_site/404.njk')
+  //   //       // Add 404 http status code in request header.
+  //   //       res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" })
+  //   //       // Provides the 404 content without redirect.
+  //   //       res.write(content_404)
+  //   //       res.end()
+  //   //     })
+  //   //   }
+  //   // },
+  //   // snippet: false,
+  // })
 
   eleventyConfig.addNunjucksAsyncShortcode(
     "imgc",
