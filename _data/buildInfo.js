8b5725f5c9d20808c659cdf73e964a37e34f7494
Bryce Wray <bwray@wraytx.com>
2022-09-01 08:58:16 -0500

    Lastmod :(

--- _data/buildInfo.js
+++ _data/buildInfo.js
@@ -1,13 +1,16 @@
+// https://www.aleksandrhovhannisyan.com/blog/eleventy-build-info/
+
 const childProcess = require('child_process');
+const { DateTime } = require("luxon")
 
 module.exports = () => {
   const now = new Date();
-  const timeZone = 'America/Chicago';
-  const buildTime = new Intl.DateTimeFormat('en-US', {
-    dateStyle: 'full',
-    timeStyle: 'short',
-    timeZone,
-  }).format(now);
+  // const timeZone = 'America/Chicago';
+  // const buildTime = new Intl.DateTimeFormat('en-US', {
+  //   dateStyle: 'full',
+  //   timeStyle: 'short',
+  //   timeZone,
+  // }).format(now);
 
   const latestGitCommitHash =
     childProcess
@@ -15,10 +18,13 @@ module.exports = () => {
     .toString()
     .trim();
 
+	let commitTime = DateTime.fromJSDate(now, { zone: 'America/Chicago' }).toFormat("yyyy-MM-dd h:mm:ss a (ZZZZ)")
+
   return {
     time: {
       raw: now.toISOString(),
-      formatted: `${buildTime} ${timeZone}`,
+      // formatted: `${buildTime} ${timeZone}`,
+			formatted: `${commitTime}`
     },
     hash: latestGitCommitHash,
   };
