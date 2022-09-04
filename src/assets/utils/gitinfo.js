e5ff1da2fc679ff0802d80ab8262e11c7ccf40dd
Bryce Wray <bwray@wraytx.com>
2022-09-04 05:49:07 -0500

    Lower (unneeded) CPU use in dev mode

--- src/assets/utils/gitinfo.js
+++ src/assets/utils/gitinfo.js
@@ -4,42 +4,49 @@
 // const spawn = require('cross-spawn')
 const { DateTime } = require("luxon")
 const childProcess = require('child_process');
+const environment = process.env.NODE_ENV
 
 module.exports = (pubdate, filename) => {
 
 	let stringtoRet = ``
 
-	let repoLink = `https://github.com/brycewray/eleventy_site/commit/`
+	if (environment === "production") {
 
-	pubdate = DateTime.fromJSDate(pubdate).toFormat("yyyy-MM-dd")
+		let repoLink = `https://github.com/brycewray/eleventy_site/commit/`
 
-	const lastUpdatedFromGit =
+		pubdate = DateTime.fromJSDate(pubdate).toFormat("yyyy-MM-dd")
+
+		const lastUpdatedFromGit =
+			childProcess
+			.execSync(`git log -1 --format=%cd --date=short ${filename}`)
+			.toString()
+			.trim()
+
+		const abbrevHash =
+			childProcess
+			.execSync(`git log -1 --pretty=format:"%h" ${filename}`)
+			.toString()
+			.trim()
+
+		const longHash =
 		childProcess
-		.execSync(`git log -1 --format=%cd --date=short ${filename}`)
-		.toString()
-		.trim()
+			.execSync(`git log -1 --pretty=format:"%H" ${filename}`)
+			.toString()
+			.trim()
 
-  const abbrevHash =
-    childProcess
-    .execSync(`git log -1 --pretty=format:"%h" ${filename}`)
-    .toString()
-    .trim()
+		repoLink += longHash
 
-	const longHash =
-	childProcess
-		.execSync(`git log -1 --pretty=format:"%H" ${filename}`)
-		.toString()
-		.trim()
-
-	repoLink += longHash
-
-	if (longHash !== '') {
-		stringtoRet = `Latest commit: <a class="mono" href="${repoLink}" rel="noopener">${abbrevHash}</a>`
-		if (pubdate !== lastUpdatedFromGit) {
-			stringtoRet += `, ${lastUpdatedFromGit}`
+		if (longHash !== '') {
+			stringtoRet = `Latest commit: <a class="mono" href="${repoLink}" rel="noopener">${abbrevHash}</a>`
+			if (pubdate !== lastUpdatedFromGit) {
+				stringtoRet += `, ${lastUpdatedFromGit}`
+			}
+		} else {
+			stringtoRet = `&nbsp;`
 		}
+
 	} else {
-		stringtoRet = `&nbsp;`
+		stringtoRet = `[Git info will appear here in production.]`
 	}
 
 	return stringtoRet
