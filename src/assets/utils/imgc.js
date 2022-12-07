cff75cbb9b419c5e32f358f8a5ab14291dc3e60e
Bryce Wray <bwray@wraytx.com>
2022-12-07 15:30:17 -0600

    Fix glitch with LQIP vars (same as in Hugo repo)

--- src/assets/utils/imgc.js
+++ src/assets/utils/imgc.js
@@ -19,7 +19,7 @@ const md5 = require('md5')
 
 const respSizes = [ 320, 640, 960, 1280, 1600, 1920 ]
 let cloudiBase = 'https://res.cloudinary.com/brycewray-com/image/upload/'
-let LQIPholder = 'f_jpg,q_1,w_20/' // note ending slash and leading zero in `q`
+let LQIPholder = 'f_jpg,q_01,w_20/' // note ending slash and leading zero in `q`
 let xFmPart1 = 'f_auto,q_auto:eco,w_'
 let xFmPart2 = ',x_0,z_1/' // note ending slash
 
