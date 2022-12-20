3509e7ec6d430986568779459030ed56f4990973
Bryce Wray <bwray@wraytx.com>
2022-12-20 11:37:19 -0600

    Handle future times (+ drafts) in dev mode, too

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,20 +1,25 @@
 // for excluding "future" content from production builds
 // https://saneef.com/tutorials/hiding-posts-with-future-dates-in-eleventy/
+// ... but also in dev
+// https://github.com/11ty/eleventy/issues/2060#issuecomment-955777844
 
 const isPageFromFuture = ({ date }) =>
-  process.env.ELEVENTY_ENV === "production" && date.getTime() > Date.now()
+  // process.env.ELEVENTY_ENV === "production" && date.getTime() > Date.now()
+	date.getTime() > Date.now()
 
 module.exports = {
   permalink: (data) => {
     const { permalink, page } = data
-    if (isPageFromFuture(page)) return false
-
+    if (isPageFromFuture(page) || data.draft) {
+			return false
+		}
     return permalink
   },
   eleventyExcludeFromCollections: (data) => {
     const { eleventyExcludeFromCollections, page } = data
-    if (isPageFromFuture(page)) return true
-
+    if (isPageFromFuture(page) || data.draft) {
+			return true
+		}
     return eleventyExcludeFromCollections
   },
 }
